81eba6807a74da502c850c679482f3a617da8ccd
bagii <bagii@nixos.local>
2026-09-02 00:51:32 +0200

    set limited generations after boot wipe

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,7 @@
   # --- Boot ---
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
 
   networking.hostName = "nixos";
 
