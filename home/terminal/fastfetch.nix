80b71d63c6ea4a1eb4f6e173f31881bd846eb8a3
bagii <bagii@nixos.local>
2026-09-23 14:12:44 +0200

    Kitty config

--- home/terminal/fastfetch.nix
+++ home/terminal/fastfetch.nix
@@ -40,7 +40,6 @@
 
       modules = [
         "title"
-        "separator"
 
         {
           type = "cpu";
--- home/terminal/kitty.nix
+++ home/terminal/kitty.nix
@@ -13,6 +13,10 @@
       background_opacity = "0.75";
       dynamic_background_opacity = "yes";
       background_blur = "1";
+
+      window_padding_width = 12;
+      placement_strategy = "center";
+
       confirm_os_window_close = "0";
     };
   };
